ca0ef0354f364f381a9814210323208ce1d43641
AktoFredy <workmailAkto@gmail.com>
2023-10-05 22:50:38 +0700

    delete fragment search

--- app/src/main/java/com/aktofredy/anilist/MainActivity.kt
+++ app/src/main/java/com/aktofredy/anilist/MainActivity.kt
@@ -5,14 +5,13 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.aktofredy.anilist.databinding.ActivityMainBinding
-import com.aktofredy.anilist.home.HomeFragmentDirections
+import com.aktofredy.anilist.search.SearchActivity
 import com.aktofredy.anilist.settings.SettingActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
@@ -21,7 +20,6 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private var state = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,11 +32,9 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_main)
 
         navController.addOnDestinationChangedListener { _, destinationRoute, _ ->
-            if (destinationRoute.id == R.id.detailActivity || destinationRoute.id == R.id.searchFragment) {
+            if (destinationRoute.id == R.id.detailActivity) {
                 binding.navBotView.visibility = View.GONE
-                state = false
             } else {
-                state = true
                 binding.navBotView.visibility = View.VISIBLE
             }
         }
@@ -56,18 +52,14 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        if (state) {
-            menuInflater.inflate(R.menu.home_app_menu, menu)
-        }
+        menuInflater.inflate(R.menu.home_app_menu, menu)
         return super.onCreateOptionsMenu(menu)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when(item.itemId) {
             R.id.search_btn -> {
-                val dir = HomeFragmentDirections.actionNavHomeToSearchFragment()
-                findNavController(R.id.nav_host_fragment_main).navigate(dir)
-                Toast.makeText(this, "WOWOW", Toast.LENGTH_SHORT).show()
+                startActivity(Intent(this, SearchActivity::class.java))
             }
             R.id.settings_btn -> {
                 startActivity(Intent(this, SettingActivity::class.java))
