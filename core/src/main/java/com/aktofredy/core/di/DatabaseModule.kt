05c0762b658851998464c9a8d0830216ce983c75
AktoFredy <workmailAkto@gmail.com>
2023-10-10 23:59:58 +0700

    database encryption done

--- core/src/main/java/com/aktofredy/core/di/DatabaseModule.kt
+++ core/src/main/java/com/aktofredy/core/di/DatabaseModule.kt
@@ -9,6 +9,8 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
 import javax.inject.Singleton
 
 @Module
@@ -17,10 +19,14 @@ class DatabaseModule {
 
     @Singleton
     @Provides
-    fun provideDatabase(@ApplicationContext context: Context): AnimeDatabase = Room.databaseBuilder(
-        context,
-        AnimeDatabase::class.java, "Anime.db"
-    ).fallbackToDestructiveMigration().build()
+    fun provideDatabase(@ApplicationContext context: Context): AnimeDatabase {
+        val passphrase: ByteArray = SQLiteDatabase.getBytes("aktofredy".toCharArray())
+        val factory = SupportFactory(passphrase)
+        return Room.databaseBuilder(
+            context,
+            AnimeDatabase::class.java, "Anime.db"
+        ).fallbackToDestructiveMigration().openHelperFactory(factory).build()
+    }
 
     @Provides
     fun provideAnimeDao(database: AnimeDatabase): AnimeDao = database.animeDao()
